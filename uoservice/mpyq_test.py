61d188a5e298528328009fe6e2e3c27f6336a63b
kimbring2 <kimbring2@gmail.com>
2023-06-06 15:15:49 +0900

    deserialize the byte array to GRPC message

--- uoservice/mpyq_test.py
+++ uoservice/mpyq_test.py
@@ -1,33 +1,27 @@
 import struct
-from mpyq import MPQArchive
 import numpy as np
+import grpc
+
+from mpyq import MPQArchive
+import UoService_pb2
+import UoService_pb2_grpc
 
 archive = MPQArchive('/home/kimbring2/ClassicUO/bin/dist/Replay/kimbring2-2023-6-6-01-56-41.uoreplay')
 
 files = archive.extract()
 
-'''
-mobileDataArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.mobileDataLen");
-equippedItemArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.equippedItemLen");
-backpackItemArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.backpackitemLen");
-corpseItemArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.corpseItemLen");
-popupMenuArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.popupMenuLen");
-clilocDataArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.clilocDataLen");
 
-playerMobileObjectArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.playerMobileObjectLen");
-mobileObjectArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.mobileObjectLen");
-itemObjectArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.itemObjectLen");
-itemDropableLandArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.itemDropableLandSimpleLen");
-vendorItemObjectArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.vendorItemObjectLen");
+def ConvertByteArrayToIntList(byteArray):
+	intList = []
 
-playerStatusZeroLenStepArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.playerStatusZeroLenStep");
-playerSkillListArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.playerSkillListLen");
+	for i in range(0, len(byteArray), np.dtype(np.uint32).itemsize):
+		intValue = struct.unpack('I', byteArray[i:i + 4])[0]
+		intList.append(intValue)
+	
+	return intList
 
-staticObjectInfoListLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.staticObjectInfoListArraysLen");
-'''
 
-test_byte_array = archive.read_file('replay.metadata.mobileDataLen')
-
+## Read the length byte array for data array
 mobileDataArrayLengthArrRead = archive.read_file("replay.metadata.mobileDataLen");
 equippedItemArrayLengthArrRead = archive.read_file("replay.metadata.equippedItemLen");
 backpackItemArrayLengthArrRead = archive.read_file("replay.metadata.backpackitemLen");
@@ -36,28 +30,197 @@ popupMenuArrayLengthArrRead = archive.read_file("replay.metadata.popupMenuLen");
 clilocDataArrayLengthArrRead = archive.read_file("replay.metadata.clilocDataLen");
 
 playerMobileObjectArrayLengthArrRead = archive.read_file("replay.metadata.playerMobileObjectLen");
-mobileObjectArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.mobileObjectLen");
-itemObjectArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.itemObjectLen");
-itemDropableLandArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.itemDropableLandSimpleLen");
-vendorItemObjectArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.vendorItemObjectLen");
+mobileObjectArrayLengthArrRead = archive.read_file("replay.metadata.mobileObjectLen");
+itemObjectArrayLengthArrRead = archive.read_file("replay.metadata.itemObjectLen");
+itemDropableLandArrayLengthArrRead = archive.read_file("replay.metadata.itemDropableLandSimpleLen");
+vendorItemObjectArrayLengthArrRead = archive.read_file("replay.metadata.vendorItemObjectLen");
 
-playerStatusZeroLenStepArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.playerStatusZeroLenStep");
-playerSkillListArrayLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.playerSkillListLen");
+playerStatusZeroLenStepArrRead = archive.read_file("replay.metadata.playerStatusZeroLenStep");
+playerSkillListArrayLengthArrRead = archive.read_file("replay.metadata.playerSkillListLen");
 
-staticObjectInfoListLengthArrRead = ReadFromMpqArchive(_replayName, "replay.metadata.staticObjectInfoListArraysLen");
-
-def ConvertByteArrayToIntList(byte_array):
-	uint32_list = []
-
-	#for (i = 0; i < len(byte_array); i += sizeof(int))
-	for i in range(0, len(byte_array), np.dtype(np.uint32).itemsize):
-		#byte_array = byte_array[i:i + 4]
-		#print("byte_array: ", byte_array)
-
-		uint32_value = struct.unpack('I', byte_array[i:i + 4])[0]
-		uint32_list.append(uint32_value)
-	
-	return uint32_list
+staticObjectInfoListLengthArrRead = archive.read_file("replay.metadata.staticObjectInfoListArraysLen");
 
 
-#test_int_list = ConvertByteArrayToIntList(test_byte_array)
+## Convert the byte array to int array
+mobileDataArrayLengthListRead = ConvertByteArrayToIntList(mobileDataArrayLengthArrRead);
+print("len(mobileDataArrayLengthListRead): ", len(mobileDataArrayLengthListRead))
+
+equippedItemArrayLengthListRead = ConvertByteArrayToIntList(equippedItemArrayLengthArrRead);
+print("len(equippedItemArrayLengthListRead): ", len(equippedItemArrayLengthListRead))
+
+backpackItemArrayLengthListRead = ConvertByteArrayToIntList(backpackItemArrayLengthArrRead);
+print("len(backpackItemArrayLengthListRead): ", len(backpackItemArrayLengthListRead))
+
+corpseItemArrayLengthListRead = ConvertByteArrayToIntList(corpseItemArrayLengthArrRead);
+print("len(corpseItemArrayLengthListRead): ", len(corpseItemArrayLengthListRead))
+
+popupMenuArrayLengthListRead = ConvertByteArrayToIntList(popupMenuArrayLengthArrRead);
+print("len(popupMenuArrayLengthListRead): ", len(popupMenuArrayLengthListRead))
+
+clilocDataArrayLengthListRead = ConvertByteArrayToIntList(clilocDataArrayLengthArrRead);
+print("len(clilocDataArrayLengthListRead): ", len(clilocDataArrayLengthListRead))
+
+playerMobileObjectArrayLengthListRead = ConvertByteArrayToIntList(playerMobileObjectArrayLengthArrRead);
+print("len(playerMobileObjectArrayLengthListRead): ", len(playerMobileObjectArrayLengthListRead))
+
+mobileObjectArrayLengthListRead = ConvertByteArrayToIntList(mobileObjectArrayLengthArrRead);
+print("len(mobileDataArrayLengthListRead): ", len(mobileDataArrayLengthListRead))
+
+itemObjectArrayLengthListRead = ConvertByteArrayToIntList(itemObjectArrayLengthArrRead);
+print("len(itemObjectArrayLengthListRead): ", len(itemObjectArrayLengthListRead))
+
+itemDropableLandArrayLengthListRead = ConvertByteArrayToIntList(itemDropableLandArrayLengthArrRead);
+print("len(itemDropableLandArrayLengthListRead): ", len(itemDropableLandArrayLengthListRead))
+
+vendorItemObjectArrayLengthListRead = ConvertByteArrayToIntList(vendorItemObjectArrayLengthArrRead);
+print("len(vendorItemObjectArrayLengthListRead): ", len(vendorItemObjectArrayLengthListRead))
+
+playerStatusZeroLenStepListRead = ConvertByteArrayToIntList(playerStatusZeroLenStepArrRead);
+print("len(playerStatusZeroLenStepListRead): ", len(playerStatusZeroLenStepListRead))
+
+playerSkillListArrayLengthListRead = ConvertByteArrayToIntList(playerSkillListArrayLengthArrRead);
+print("len(playerSkillListArrayLengthListRead): ", len(playerSkillListArrayLengthListRead))
+
+staticObjectInfoListLengthListRead = ConvertByteArrayToIntList(staticObjectInfoListLengthArrRead);
+print("len(staticObjectInfoListLengthListRead): ", len(staticObjectInfoListLengthListRead))
+
+
+## Read the actual for data array
+mobileDataArrRead = archive.read_file("replay.data.mobileData");
+equippedItemArrRead = archive.read_file("replay.data.equippedItem");
+backpackItemArrRead = archive.read_file("replay.data.backpackItem");
+corpseItemArrRead = archive.read_file("replay.data.corpseItem");
+popupMenuArrRead = archive.read_file("replay.data.popupMenu");
+clilocDataArrRead = archive.read_file("replay.data.clilocData");
+
+playerMobileObjectArrRead = archive.read_file("replay.data.playerMobileObject");
+mobileObjectArrRead = archive.read_file("replay.data.mobileObject");
+itemObjectArrRead = archive.read_file("replay.data.itemObject");
+itemDropableLandArrRead = archive.read_file("replay.data.itemDropableLandSimple");
+vendorItemObjectArrRead = archive.read_file("replay.data.vendorItemObject");
+
+playerStatusArrRead = archive.read_file("replay.data.playerStatus");
+playerSkillListArrRead = archive.read_file("replay.data.playerSkillList");
+
+staticObjectInfoListArrRead = archive.read_file("replay.data.staticObjectInfoList");
+
+
+## Check the data array is existed
+if mobileDataArrRead:
+	print("len(mobileDataArrRead): ", len(mobileDataArrRead))
+else:
+	print("mobileDataArrRead is None")
+
+if equippedItemArrRead:
+	print("len(equippedItemArrRead): ", len(equippedItemArrRead))
+else:
+	print("equippedItemArrRead is None")
+
+if backpackItemArrRead:
+	print("len(backpackItemArrRead): ", len(backpackItemArrRead))
+else:
+	print("backpackItemArrRead is None")
+
+if corpseItemArrRead:
+	print("len(corpseItemArrRead): ", len(corpseItemArrRead))
+else:
+	print("corpseItemArrRead is None")
+
+if popupMenuArrRead:
+	print("len(popupMenuArrRead): ", len(popupMenuArrRead))
+else:
+	print("popupMenuArrRead is None")
+
+if clilocDataArrRead:
+	print("len(clilocDataArrRead): ", len(clilocDataArrRead))
+else:
+	print("clilocDataArrRead is None")
+
+if playerMobileObjectArrRead:
+	print("len(playerMobileObjectArrRead): ", len(playerMobileObjectArrRead))
+else:
+	print("playerMobileObjectArrRead is None")
+
+if mobileObjectArrRead:
+	print("len(mobileObjectArrRead): ", len(mobileObjectArrRead))
+else:
+	print("mobileObjectArrRead is None")
+
+if itemObjectArrRead:
+	print("len(itemObjectArrRead): ", len(itemObjectArrRead))
+else:
+	print("itemObjectArrRead is None")
+
+if itemDropableLandArrRead:
+	print("len(itemDropableLandArrRead): ", len(itemDropableLandArrRead))
+else:
+	print("itemDropableLandArrRead is None")
+
+if vendorItemObjectArrRead:
+	print("len(vendorItemObjectArrRead): ", len(vendorItemObjectArrRead))
+else:
+	print("vendorItemObjectArrRead is None")
+
+if playerStatusArrRead:
+	print("len(playerStatusArrRead): ", len(playerStatusArrRead))
+else:
+	print("playerStatusArrRead is None")
+
+if playerSkillListArrRead:
+	print("len(playerSkillListArrRead): ", len(playerSkillListArrRead))
+else:
+	print("playerSkillListArrRead is None")
+
+if staticObjectInfoListArrRead:
+	print("len(staticObjectInfoListArrRead): ", len(staticObjectInfoListArrRead))
+else:
+	print("staticObjectInfoListArrRead is None")
+
+
+## Initialize the length related variable
+_replayStep = len(mobileDataArrayLengthListRead)
+
+_mobileDataArrayOffset = 0
+_equippedItemArrayOffset = 0
+_backpackItemArrayOffset = 0
+_corpseItemArrayOffset = 0
+_popupMenuArrayOffset = 0
+_clilocDataArrayOffset = 0
+
+_playerMobileObjectArrayOffset = 0
+_mobileObjectArrayOffset = 0
+_itemObjectArrayOffset = 0
+_itemDropableLandArrayOffset = 0
+_vendorItemObjectArrayOffset = 0
+
+_playerStatusArrayOffset = 0
+_playerSkillListArrayOffset = 0
+
+_staticObjectInfoListArrayOffset = 0
+
+
+def GetSubsetArray(index, lengthListRead, offset, arrRead):
+	item = lengthListRead[index];
+	#print("item: ", item)
+
+	startIndex = offset; 
+	#Array.Copy(arrRead, startIndex, subsetArray, 0, item);
+	subsetArray = arrRead[startIndex:startIndex + item]
+	#print("subsetArray: ", subsetArray)
+
+	offset += item;
+
+	return subsetArray, offset;
+
+
+for step in range(0, _replayStep):
+	mobileDataSubsetArray, _mobileDataArrayOffset = GetSubsetArray(step, mobileDataArrayLengthListRead, _mobileDataArrayOffset, mobileDataArrRead)
+
+	#print("mobileDataSubsetArray: ", mobileDataSubsetArray)
+
+	message = UoService_pb2.GrpcMobileList()
+	grpcMobileDataReplay = message.FromString(mobileDataSubsetArray);
+	print("grpcMobileDataReplay: ", grpcMobileDataReplay)
+	#print("")
+
+	#states.MobileList = grpcMobileDataReplay;
